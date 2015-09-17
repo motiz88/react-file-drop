ba6e5680ae19a8cc85a826faa69dba1b6ad25304
motiz88 <motiz88@gmail.com>
2015-09-17 17:10:31 +0300

    Make isomorphic use a little simpler.

--- FileDrop.js
+++ FileDrop.js
@@ -3,13 +3,11 @@
         var React = require("react");
         var objectAssign = require('object-assign');
         module.exports = factory(React, objectAssign);
-    }
-    else if (typeof define === "function" && define.amd) {
+    } else if (typeof define === "function" && define.amd) {
         define(["react", "object-assign"], function(React, objectAssign) {
             return factory(React, objectAssign);
         });
-    }
-    else {
+    } else {
         factory(root.React, root.objectAssign);
     }
 }(this, function(React, objectAssign) {
@@ -23,12 +21,12 @@
             onDragLeave: React.PropTypes.func,
             dropEffect: React.PropTypes.oneOf(["copy", "move", "link", "none"]),
             targetAlwaysVisible: React.PropTypes.bool,
-            frame: function (props, propName, componentName) {
+            frame: function(props, propName, componentName) {
                 var prop = props[propName];
                 if (prop == null) {
                     return new Error("Warning: Required prop `" + propName + "` was not specified in `" + componentName + "`");
                 }
-                if (prop !== document && prop !== window && !(prop instanceof HTMLElement)) {
+                if (prop !== global.document && prop !== global && !(prop instanceof HTMLElement)) {
                     return new Error("Warning: Prop `" + propName + "` must be one of the following: document, window, or an HTMLElement!");
                 }
             },
@@ -43,30 +41,40 @@
             })
         },
 
-        getDefaultProps: function () {
+        getDefaultProps: function() {
             return {
                 dropEffect: "copy",
-                frame: document,
+                frame: global.document,
                 targetAlwaysVisible: false
             };
         },
 
         // getInitialState: in componentWillMount, we call this.resetDragging();
 
-        resetDragging: function () {
-            this._dragCount = 0;
-            this.setState({draggingOverFrame: false, draggingOverTarget: false});
+        getInitialState: function() {
+            return {
+                draggingOverFrame: false,
+                draggingOverTarget: false
+            }
         },
 
-        _handleDrop: function (event) {
+        resetDragging: function() {
+            this._dragCount = 0;
+            this.setState({
+                draggingOverFrame: false,
+                draggingOverTarget: false
+            });
+        },
+
+        _handleDrop: function(event) {
             event.preventDefault();
             if (this.props.onDrop) {
-                var files = (event.dataTransfer) ? event.dataTransfer.files : (event.frame) ? event.frame.files : undefined;
+                var files = (event.dataTransfer) ? event.dataTransfer.files : (event.frame) ? event.frame.files : event.target.files;
                 this.props.onDrop(files, event);
             }
         },
 
-        _handleDragOver: function (event) {
+        _handleDragOver: function(event) {
             event.preventDefault();
             event.stopPropagation();
             event.dataTransfer.dropEffect = this.props.dropEffect;
@@ -75,28 +83,36 @@
             // (in mozilla when file is dragged effect is "uninitialized")
             var effectAllowed = event.dataTransfer.effectAllowed;
             if (effectAllowed === "all" || effectAllowed === "uninitialized") {
-                this.setState({draggingOverTarget: true});
+                this.setState({
+                    draggingOverTarget: true
+                });
             }
 
             if (this.props.onDragOver) this.props.onDragOver(event);
         },
 
-        _handleDragLeave: function (event) {
-            this.setState({draggingOverTarget: false});
+        _handleDragLeave: function(event) {
+            this.setState({
+                draggingOverTarget: false
+            });
             if (this.props.onDragLeave) this.props.onDragLeave(event);
         },
 
-        _handleFrameDrag: function (event) {
+        _handleFrameDrag: function(event) {
             // We are listening for events on the 'frame', so every time the user drags over any element in the frame's tree,
             // the event bubbles up to the frame. By keeping count of how many "dragenters" we get, we can tell if they are still
             // "draggingOverFrame" (b/c you get one "dragenter" initially, and one "dragenter"/one "dragleave" for every bubble)
             this._dragCount += (event.type === "dragenter" ? 1 : -1);
             if (this._dragCount === 1) {
-                this.setState({draggingOverFrame: true});
+                this.setState({
+                    draggingOverFrame: true
+                });
                 if (this.props.onFrameDragEnter) this.props.onFrameDragEnter(event);
             } else if (this._dragCount === 0) {
                 if (this.props.onFrameDragLeave) this.props.onFrameDragLeave(event);
-                this.setState({draggingOverFrame: false});
+                this.setState({
+                    draggingOverFrame: false
+                });
             }
         },
 
@@ -105,21 +121,36 @@
             if (this.props.onFrameDrop) this.props.onFrameDrop(event);
         },
 
-        render: function () {
+        render: function() {
             var fileDropTarget;
             var fileDropTargetStyles = [{}, (this.props.dropTargetStyles || {}).base];
             if (this.props.targetAlwaysVisible || this.state.draggingOverFrame) {
                 fileDropTargetStyles.push((this.props.dropTargetStyles || {}).draggingOverFrame);
                 if (this.state.draggingOverTarget) fileDropTargetStyles.push((this.props.dropTargetStyles || {}).draggingOverTarget);
                 fileDropTarget = (
-                    React.createElement("div", {style: objectAssign.apply(null, fileDropTargetStyles)},
+                    React.createElement("div", {
+                            style: objectAssign.apply(null, fileDropTargetStyles)
+                        },
                         this.props.children
                     )
                 );
             }
             return (
-                React.createElement("div", {style: this.props.style, onDrop: this._handleDrop, onDragLeave: this._handleDragLeave, onDragOver: this._handleDragOver},
-                    fileDropTarget
+                React.createElement("div", {
+                        style: this.props.style,
+                        onDrop: this._handleDrop,
+                        onDragLeave: this._handleDragLeave,
+                        onDragOver: this._handleDragOver
+                    },
+                    fileDropTarget,
+                    React.createElement("input", {
+                        type: 'file',
+                        ref: 'fileInput',
+                        style: {
+                            display: 'none'
+                        },
+                        onChange: this._handleDrop
+                    })
                 )
             );
         },
@@ -138,7 +169,7 @@
 
         componentWillMount: function() {
             this.startFrameListeners();
-            this.resetDragging();
+            this._dragCount = 0;
             window.addEventListener("dragover", this._handleWindowDragOverOrDrop);
             window.addEventListener("drop", this._handleWindowDragOverOrDrop);
         },
@@ -151,17 +182,27 @@
 
         stopFrameListeners: function(frame) {
             frame = frame || this.props.frame;
-            frame.removeEventListener("dragenter", this._handleFrameDrag);
-            frame.removeEventListener("dragleave", this._handleFrameDrag);
-            frame.removeEventListener("drop", this._handleFrameDrop);
+            if (frame) {
+                frame.removeEventListener("dragenter", this._handleFrameDrag);
+                frame.removeEventListener("dragleave", this._handleFrameDrag);
+                frame.removeEventListener("drop", this._handleFrameDrop);
+            }
         },
 
         startFrameListeners: function(frame) {
             frame = frame || this.props.frame;
-            frame.addEventListener("dragenter", this._handleFrameDrag);
-            frame.addEventListener("dragleave", this._handleFrameDrag);
-            frame.addEventListener("drop", this._handleFrameDrop);
-        }
+            if (frame) {
+                frame.addEventListener("dragenter", this._handleFrameDrag);
+                frame.addEventListener("dragleave", this._handleFrameDrag);
+                frame.addEventListener("drop", this._handleFrameDrop);
+            }
+        },
+
+        open: function() {
+            var fileInput = React.findDOMNode(this.refs.fileInput);
+            fileInput.value = null;
+            fileInput.click();
+        },
     });
 
     if (typeof exports === "undefined" && typeof define !== "function" && !this.ReactFileDrop) {
